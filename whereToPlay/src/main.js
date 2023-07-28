0500a9d50effcfcdef57d8efb57d82c2b7a77b0f
koala0018 <1403019363@qq.com>
2023-07-28 17:28:04 +0800

    2023-7-28 切换为新预设-big-circle

--- whereToPlay/src/main.js
+++ whereToPlay/src/main.js
@@ -5,23 +5,18 @@ import router from './router'
 import {init as naiveInit} from './plugins/naive'
 import {init as piniaInit} from './plugins/naive'
 import setupAssets from "./plugins/setAsset";
-import VueKinesis from "vue-kinesis";
-import Particles from "particles.vue3";
-// import VueParticles from 'vue-particles'
-const setupApp = async () =>{
-    const app = createApp(App);
-    naiveInit(app);
-    piniaInit(app);
-    setupAssets();
-    app.use(router);
-    app.use(VueKinesis);
-    app.use(Particles);
-    // app.use(VueParticles);
-    app.mount('#app')
-}
+import Particles from 'particles.vue3'
 
 
-setupApp();
+const app = createApp(App);
+naiveInit(app);
+piniaInit(app);
+setupAssets();
+app.use(router);
+app.use(Particles);
+app.mount('#app')
+
+
 
 
 
